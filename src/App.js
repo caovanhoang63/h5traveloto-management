da04587dcd63cd1dcd46efaff15a394ce91ace1d
BuiTrongHoangHuy <22520533@gm.uit.edu.vn>
2024-03-20 20:51:53 +0700

    add textbox and combobox components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import ComboBox from './components/combobox/combobox';
+import Textbox from './components/textbox/textbox';
 function App() {
+  
+const options = [
+  { value: 'tuoi' },
+  { value: 'báo'},
+  { value: 's' },
+];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+    <Textbox placeHolder="Nhập dữ liệu"
+      title="Dữ liệu"
+    />
+    <ComboBox label="lựa chọn giá đúng" options = {options}></ComboBox>
     </div>
   );
 }
-
+ 
 export default App;
