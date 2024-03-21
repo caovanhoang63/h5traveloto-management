8b8125a0d6a5504d3556e559634df87fb17cc2db
hungmai <126455648+mhhung0811@users.noreply.github.com>
2024-03-21 20:01:22 +0700

    update arrow: it can spin now!

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -2,15 +2,8 @@ import img_Logo from "../../assets/logo.png"
 import img_LogoFull from "../../assets/logofull.png"
 import { useState } from "react";
 import "./sidebar.css";
-import { 
-	ArrowLineLeft,
-	House, 
-	NotePencil, 
-	Users, 
-	BookmarkSimple,
-	Tag,
-	CurrencyCircleDollar
-} from "@phosphor-icons/react/dist/ssr";
+import ico_arrow_line_left from "../../assets/icons/arrow-line-left.png";
+import ico_arrow_line_left_active from "../../assets/icons/arrow-line-left-active.png";
 import Sidebar_tab from "./sidebar_tab";
 
 const Sidebar = ({data}) => {
@@ -48,7 +41,7 @@ const Sidebar = ({data}) => {
 		<div className="container">
 			<div className={'sidebar' + (isToggleSideBar ? ' active' : '') }>
 				<div className="menu-btn" onClick={handleClick_MenuBtn}>
-					<ArrowLineLeft size={16}/>
+					<img src={ico_arrow_line_left} alt="" />
 				</div>
 				<div className="head">
 					<div className={'logo' + (isToggleSideBar ? ' active' : '')}>
@@ -71,3 +64,76 @@ const Sidebar = ({data}) => {
 }
  
 export default Sidebar;
+
+// import Sidebar from './components/sidebar/sidebar';
+// import ico_home from "./assets/icons/home.png";
+// import ico_home_active from "./assets/icons/home-active.png";
+// import ico_edit from "./assets/icons/edit.png";
+// import ico_edit_active from "./assets/icons/edit-active.png";
+// import ico_check_file from "./assets/icons/check-file.png";
+// import ico_check_file_active from "./assets/icons/check-file-active.png";
+// import ico_bookmark from "./assets/icons/bookmark.png";
+// import ico_bookmark_active from "./assets/icons/bookmark-active.png";
+// import ico_tag from "./assets/icons/tag.png";
+// import ico_tag_active from "./assets/icons/tag-active.png";
+// import ico_dollar_circle from "./assets/icons/dollar-circle.png";
+// import ico_dollar_circle_active from "./assets/icons/dollar-circle-active.png";
+
+// const sidebar_data = [
+//   {
+//     id: 0,
+//     ico: ico_home,
+//     active_ico: ico_home_active,
+//     content: "Dashboard",
+//     isToggle: true,
+//     isActive: false
+//   },
+//   {
+//     id: 1,
+//     ico: ico_edit,
+//     active_ico: ico_edit_active,
+//     content: "Front desk",
+//     isToggle: true,
+//     isActive: false
+//   },
+//   {
+//     id: 2,
+//     ico: ico_check_file,
+//     active_ico: ico_check_file_active,
+//     content: "Guest",
+//     isToggle: true,
+//     isActive: false
+//   },
+//   {
+//     id: 3,
+//     ico: ico_bookmark,
+//     active_ico: ico_bookmark_active,
+//     content: "Rooms",
+//     isToggle: true,
+//     isActive: false
+//   },
+//   {
+//     id: 4,
+//     ico: ico_tag,
+//     active_ico: ico_tag_active,
+//     content: "Deal",
+//     isToggle: true,
+//     isActive: false
+//   },
+//   {
+//     id: 5,
+//     ico: ico_dollar_circle,
+//     active_ico: ico_dollar_circle_active,
+//     content: "Rate",
+//     isToggle: true,
+//     isActive: false
+//   }
+// ];
+
+// function App() {
+//   return (
+//     <div>
+//       <Sidebar data={sidebar_data}></Sidebar>
+//     </div>
+//   );
+// }
