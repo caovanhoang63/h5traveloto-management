a1739121cd40a943e9c42fbd8dd9e620e8b50457
BuiTrongHoangHuy <22520533@gm.uit.edu.vn>
2024-03-26 16:25:17 +0700

    Update button.jsx

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,36 +2,34 @@ import React from 'react';
 import './button.css'
 const Button = ({type,className,onClick,disabled,text="",icon,src,alt,positionIcon}) => {
   if(positionIcon==="right")
-  return (
+    return (
+      <div>
+        <button
+        type={type||"button"}
+        className={`button-container ${className}`}
+        onClick={onClick}
+        disabled={disabled}>
+        <span className={(text==="" ? 'button-notext' : 'button-text')}>{text}</span>
+        <span>
+          <img  className="button-icon" src={src} alt={""||alt}></img>
+        </span>
+  </button>
+      </div>
+    );
+else return (
     <div>
       <button
       type={type||"button"}
       className={`button-container ${className}`}
       onClick={onClick}
-      disabled={disabled}
->
-      <span className={(text==="" ? 'button-notext' : 'button-text')}>{text}</span>
+      disabled={disabled}>
       <span>
         <img  className="button-icon" src={src} alt={""||alt}></img>
       </span>
-</button>
+      <span className={(text==="" ? 'button-notext' : 'button-text')}>{text}</span>
+  </button>
     </div>
   );
-else return (
-  <div>
-    <button
-    type={type||"button"}
-    className={`button-container ${className}`}
-    onClick={onClick}
-    disabled={disabled}
->
-    <span>
-      <img  className="button-icon" src={src} alt={""||alt}></img>
-    </span>
-    <span className={(text==="" ? 'button-notext' : 'button-text')}>{text}</span>
-</button>
-  </div>
-);
 } 
 
 export default Button;
