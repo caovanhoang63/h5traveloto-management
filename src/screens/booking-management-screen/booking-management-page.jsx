5362641b11fbe257299c15d5d84f73e09353235c
Bui Thai Hoang <154488027+BuiThaiHoang267@users.noreply.github.com>
2024-07-03 22:22:48 +0700

    fix nhẹ

--- src/screens/booking-management-screen/booking-management-page.jsx
+++ src/screens/booking-management-screen/booking-management-page.jsx
@@ -6,9 +6,71 @@ import {
 import "./booking-management-page.css";
 import IconFilter from "../../assets/icons/icon-filter.png";
 import TextUnderline from "../../components/textunderline/texunderline";
+import Table from "../../components/table/table";
 import { Link } from "react-router-dom";
 
 function BookingManagementPage() {
+    const columns = [
+        {
+            Header: "No.",
+            accessor: "no",
+        },
+        {
+            Header: "Room type",
+            accessor: "roomType",
+        },
+        {
+            Header: "Room Quantity",
+            accessor: "room Quantity",
+        },
+        {
+            Header: "Start Date",
+            accessor: "startDate",
+        },
+        {
+            Header: "End Date",
+            accessor: "endDate",
+        },
+    ];
+
+    const data = [
+        {
+            no: 1,
+            roomType: "Single",
+            "room Quantity": 5,
+            startDate: "Reserved",
+            endDate: "Reserved",
+        },
+        {
+            no: 2,
+            roomType: "Double",
+            "room Quantity": 10,
+            startDate: "Available",
+            endDate: "Available",
+        },
+        {
+            no: 3,
+            roomType: "Suite",
+            "room Quantity": 2,
+            startDate: "Available",
+            endDate: "Available",
+        },
+        {
+            no: 4,
+            roomType: "Single",
+            "room Quantity": 8,
+            startDate: "Booked",
+            endDate: "Booked",
+        },
+        {
+            no: 5,
+            roomType: "Double",
+            "room Quantity": 6,
+            startDate: "Reserved",
+            endDate: "Reserved",
+        },
+    ];
+
     const [focusedIndex, setFocusedIndex] = useState(0);
     const buttons = ["Check-In", "Confirm", "Pending", "Cancel & Checked-Out"];
     return (
@@ -52,6 +114,9 @@ function BookingManagementPage() {
                         </PrimaryButton>
                     </Link>
                 </div>
+            </div>
+            <div className="booking-table">
+                <Table columns={columns} data={data} />
             </div>
         </div>
     );
